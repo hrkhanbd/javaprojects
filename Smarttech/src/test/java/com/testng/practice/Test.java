502decf80700da5aca4ddde9683be986d2e1ae03
hrkhanbd <h88031@yahoo.com>
2022-05-30 04:54:04 +0100

    added xyz variable in test class Hydrabad branch

--- Smarttech/src/test/java/com/testng/practice/Test.java
+++ Smarttech/src/test/java/com/testng/practice/Test.java
@@ -1,11 +1,13 @@
 package com.testng.practice;
 
 public class Test {
+public int x =20, y =30, Z=40;
 
 	public static void main(String[] args) {
 		System.out.println("testing git");
 		System.out.println("testing git");
 		System.out.println("testing git");
+		
 
 	}
 
