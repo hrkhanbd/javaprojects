8532ab39f3de9476f228c20fcecea0cbd92f6aa8
hrkhanbd <h88031@yahoo.com>
2022-05-30 05:02:34 +0100

    added abc variables to Bengalore branch

--- Smarttech/src/test/java/com/testng/practice/Test.java
+++ Smarttech/src/test/java/com/testng/practice/Test.java
@@ -1,7 +1,7 @@
 package com.testng.practice;
 
 public class Test {
-
+public int a=100, b=200, c=400;
 	public static void main(String[] args) {
 		System.out.println("testing git");
 		System.out.println("testing git");
